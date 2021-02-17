04389c68cf9717ad3a696df33b3f07bbc2c18660
Rafael <rafaelroma@timepix.com.br>
2021-02-16 22:06:51 -0300

    ajuste repositories

--- src/Pages/repository/index.tsx
+++ src/Pages/repository/index.tsx
@@ -10,7 +10,7 @@ interface RepositoryParams {
     repository: string
 }
 
-interface Repository {
+interface Repositories {
 	full_name: string,
 	description: string,
 	forks_count: number,
@@ -32,7 +32,7 @@ interface Issue {
 }
 
 const Repository: React.FC = () => {
-    const [repository, setRepository] = useState<Repository | null>(null)
+    const [repository, setRepository] = useState<Repositories | null>(null)
     const [issues, setIssues] = useState<Issue[]>([])
     const {params} = useRouteMatch<RepositoryParams>()
 
